7a0ea42b7742aea2981360950e1a3e5d55315dc4
Daniel Rusnac <daniel.rusnac@trymygames.com>
2022-11-18 14:39:54 +0200

    Create build settings

--- CI/Packages/com.tmg.ci/Editor/BuildMode.cs
+++ CI/Packages/com.tmg.ci/Editor/BuildMode.cs
@@ -7,7 +7,19 @@ namespace Tmg.CI
     {
         public string Name;
         public string Preprocessor;
+        public bool ShowSplashScreen;
+        public bool IsDevelopment;
+        public bool CheatsEnabled;
+        public bool IncludeSdks;
 
-        public static BuildMode Default = new() { Name = "Default", Preprocessor = "" };
+        public static BuildMode Default = new()
+        {
+            Name = "Default", 
+            Preprocessor = "",
+            ShowSplashScreen = false,
+            IsDevelopment = false,
+            CheatsEnabled = false,
+            IncludeSdks = true
+        };
     }
 }
